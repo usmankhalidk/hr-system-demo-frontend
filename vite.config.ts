a1fb6ef062bbba36c893c3ed4e446702094cb769
Dev-Logic-max <ar3991492@gmail.com>
2026-04-30 19:32:59 +0500

    Error Fix

--- vite.config.ts
+++ vite.config.ts
@@ -14,11 +14,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true,
       },
     },
